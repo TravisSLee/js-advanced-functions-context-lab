2a2eed35b900bcd9508b46e340d2ea0a9c6d0037
TravisSLee <travisslee223@gmail.com>
2021-03-15 11:50:54 -0400

    commit tree 9480e58cd6a04926c2607681a057fb023e427ebb

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeInfo) {
+    let employeeRecord = {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
 
+function createEmployeeRecords(array) {
+    return array.map(createEmployeeRecord); 
+}
+
+function createTimeInEvent(obj, timeStamp) {
+    let timeInEvent = {
+        type: 'TimeIn',
+        hour: parseInt(timeStamp.split(' ')[1]),
+        date: timeStamp.split(' ')[0]
+        }
+    obj.timeInEvents.push(timeInEvent)
+    return obj
+}
+
+function createTimeOutEvent(obj, timeStamp){
+    let timeOutEvent = {
+        type: 'TimeOut',
+        hour: parseInt(timeStamp.split(' ')[1]),
+        date: timeStamp.split(' ')[0]
+        }
+    obj.timeOutEvents.push(timeOutEvent)
+    return obj
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
